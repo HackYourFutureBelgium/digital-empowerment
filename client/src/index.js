66ecb182921a4231235be69285e9f7f2b5a09c55
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-30 09:11:09 +0100

    Add normalize.css

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'normalize.css';
+import './assets/css/index.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
