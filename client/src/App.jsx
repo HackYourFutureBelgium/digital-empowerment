a7df15befd8c8a79f985729959a902c42141f610
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-30 13:54:35 +0100

    Make App component stateless

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { getModules } from './api/modules';
 import Modules from './components/Modules';
 
-class App extends Component {
-  componentDidMount() {
-    getModules().then((modules) => {
-      console.log(modules);
-    })
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path=":path(modules|)" component={Modules} />
-          <Route render={() => <p>Page not found</p>} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path=":path(modules|)" component={Modules} />
+      <Route render={() => <p>Page not found</p>} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
