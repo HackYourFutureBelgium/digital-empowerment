e4932d5634fd68ab1ef7e253e6d5389b09e2c61f
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-06 17:59:45 +0100

    Add update and delete buttons

--- client/src/components/Modules/Module.jsx
+++ client/src/components/Modules/Module.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Module = ({ module }) => (
   <article className="module">
     <h3 className="module__title">{module.title}</h3>
+    <div className="module__actions">
+      <i><FontAwesomeIcon icon={faEdit} /></i>
+      <i><FontAwesomeIcon icon={faTrash} /></i>
+    </div>
   </article>
 );
 
