1eed6352628c0edacbbc2d8af34f15ad3793e07d
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-06 14:30:26 +0100

    Style modules individually

--- client/src/components/Modules.jsx
+++ client/src/components/Modules.jsx
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import { getModules } from '../api/modules';
 
+import '../assets/css/modules.css';
+
 class Modules extends Component {
   state = {
-    modules: []
+    modules: [
+      {
+        _id: 1,
+        title: 'Home page'
+      },
+      {
+        _id: 2,
+        title: 'Google results'
+      }
+    ]
   };
 
   componentDidMount() {
     getModules().then((modules) => {
-      this.setState({ modules });
+      // this.setState({ modules });
     });
   }
 
   render() {
     const { modules } = this.state;
 
-    if (modules.length > 0) {
-      return (
-        <ul>
-          {modules.map(module => <li key={module._id}>{module.title}</li>)}
-        </ul>
-      );
-    }
-
     return (
-      <p>There are no modules yet</p>
+      <div className="container">
+        <h2>Using a web browser</h2>
+        <div className="modules">
+          { modules.length > 0
+            ? modules.map(module => (
+              <article className="module" key={module._id}>
+                <h3 className="module__title">{module.title}</h3>
+              </article>
+            ))
+            : <p>There are no modules yet</p>
+          }
+        </div>
+      </div>
     );
   }
 }
