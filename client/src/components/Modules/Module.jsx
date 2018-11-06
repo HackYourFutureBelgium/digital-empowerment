9ebb331af09fff08e9e9ccf2073eab2d80e1114e
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-06 19:41:21 +0100

    Create dedicated component for confirmation dialogs

--- client/src/components/Modules/Module.jsx
+++ client/src/components/Modules/Module.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import ConfirmationDialog from '../ConfirmationDialog';
 
 Modal.setAppElement('#root');
 
@@ -28,19 +29,14 @@ class Module extends Component {
     const { module } = this.props;
     return (
       <article className="module">
-        <Modal
+        <ConfirmationDialog
           isOpen={confirmingDeletion}
-          onRequestClose={this.cancelDeletion}
-          className="modal modal--confirmation"
-          overlayClassName="modal-overlay"
-        >
-          <h2 className="modal__title">Confirm deletion</h2>
-          <p>Are you sure you want to delete module &quot;{module.title}&quot;? </p>
-          <div className="modal--confirmation__actions">
-            <button className="button" type="button" onClick={this.cancelDeletion}>Cancel</button>
-            <button className="button" type="button" onClick={this.deleteModule}>Delete</button>
-          </div>
-        </Modal>
+          onClose={this.cancelDeletion}
+          cancel={this.cancelDeletion}
+          accept={this.deleteModule}
+          title="Confirm deletion"
+          text={`Are you sure you want to delete module "${module.title}"`}
+        />
         <h3 className="module__title">{module.title}</h3>
         <div className="module__actions">
           <i><FontAwesomeIcon icon={faEdit} /></i>
