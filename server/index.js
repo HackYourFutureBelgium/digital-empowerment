d0c39b1babafb4ceb02fcac4d92a1d017f41a441
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-07 11:16:21 +0100

    Add Heroku deploy steps

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,7 +9,7 @@ require('dotenv').config();
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => { console.log('Database connection established'); })
   .catch((err) => {
     console.error(`Database error, exiting. Stack trace:\n${err}`);
@@ -26,6 +27,14 @@ app.get('/', (req, res) => {
 });
 
 require('./src/route/module.route')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  const buildPath = path.join(`${__dirname}`, '../client/build');
+  app.use(express.static(buildPath));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${buildPath}/index.html`));
+  });
+}
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
