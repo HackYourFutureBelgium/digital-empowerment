fac85ae2304554b7d19042e79cf0f3a0905297c8
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-07 14:45:36 +0100

    Accept environment variable for API URL

--- client/src/api/modules.js
+++ client/src/api/modules.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:4000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 const headers = new Headers({
   'Content-Type': 'application/json'
