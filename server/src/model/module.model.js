fda3b144d6a3e68d33543180d07c309acf153eab
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-10 11:41:26 +0100

    Add stages to module schema

--- server/src/model/module.model.js
+++ server/src/model/module.model.js
@@ -5,7 +5,10 @@ const ModuleSchema = mongoose.Schema(
     title: {
       type: String,
       required: true
-    }
+    },
+    explanation: String,
+    exercise: String,
+    evaluation: String
   },
   {
     timestamps: true
