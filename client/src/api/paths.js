9c66a4b268fb320ea2aed0e2f87303f39f1cb0d9
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-21 16:49:18 +0100

    Add Path API functions

--- client/src/api/paths.js
+++ client/src/api/paths.js
@@ -4,3 +4,13 @@ const headers = new Headers({
   'Content-Type': 'application/json'
 });
 
+export const getPaths = () => fetch(`${API_URL}/path`).then(response => response.json());
+export const getPath = id => fetch(`${API_URL}/path/${id}`).then(response => response.json());
+
+export const createPath = body => (
+  fetch(`${API_URL}/path`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body)
+  }).then(response => response.json())
+);
