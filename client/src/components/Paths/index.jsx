303484aaa95aa68886aa1b5bfa192b09f382fc70
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-22 10:27:09 +0100

    Create paths index

--- client/src/components/Paths/index.jsx
+++ client/src/components/Paths/index.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as api from '../../api/paths';
 
-const Paths = () => <p>Paths</p>;
+class Paths extends Component {
+  state = {
+    paths: []
+  };
+
+  async componentDidMount() {
+    const paths = await api.getPaths().catch(err => console.error(err));
+    this.setState({ paths });
+  }
+
+  render() {
+    const { paths } = this.state;
+
+    const $paths = paths.map(path => <p key={path._id}>{path.title}</p>);
+    return $paths;
+  }
+}
 
 export default Paths;
