0e87675e7558c939bc0a72854a1eb5ca65b0b941
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-01 17:17:28 +0100

    Add user model

---
+++ server/src/model/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('User', UserSchema);
