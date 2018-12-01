08f493b7fa5534ff02e9b8eb5a70c22cfb5ba63f
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-01 20:01:51 +0100

    Secure routes

--- server/src/route/module.route.js
+++ server/src/route/module.route.js
@@ -1,8 +1,9 @@
+const verifyToken = require('../verify-token');
 const modules = require('../controller/module.controller.js');
 
 module.exports = (app) => {
   app.get('/module', modules.findAll);
-  app.post('/module', modules.create);
-  app.patch('/module/:moduleId', modules.update);
-  app.delete('/module/:moduleId', modules.delete);
+  app.post('/module', verifyToken, modules.create);
+  app.patch('/module/:moduleId', verifyToken, modules.update);
+  app.delete('/module/:moduleId', verifyToken, modules.delete);
 };
--- server/src/route/path.route.js
+++ server/src/route/path.route.js
@@ -1,11 +1,12 @@
+const verifyToken = require('../verify-token');
 const paths = require('../controller/path.controller');
 const modules = require('../controller/module.controller');
 
 module.exports = (app) => {
   app.get('/path', paths.findAll);
   app.get('/path/:pathId', paths.findOne);
-  app.post('/path', paths.create);
-  app.post('/path/:pathId/module', modules.create);
-  app.patch('/path/:pathId', paths.update);
-  app.delete('/path/:pathId', paths.delete);
+  app.post('/path', verifyToken, paths.create);
+  app.post('/path/:pathId/module', verifyToken, modules.create);
+  app.patch('/path/:pathId', verifyToken, paths.update);
+  app.delete('/path/:pathId', verifyToken, paths.delete);
 };
