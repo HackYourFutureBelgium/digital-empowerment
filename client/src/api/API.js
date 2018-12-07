d156853864eb3359c50bcb10fe6e012b9a720171
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-07 12:01:26 +0100

    Create API wrapper class

---
+++ client/src/api/API.js
@@ -0,0 +1,29 @@
+import { API_URL, cookies } from '../constants';
+
+class API {
+  constructor() {
+    this.BASE_URL = API_URL;
+    this.headers = new Headers({
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    });
+  }
+
+  get authHeaders() {
+    return this.headers.append('Authorization', `Bearer ${cookies.get('auth')}`);
+  }
+
+  getOptions(method, authRequired = false, body = null) {
+    const options = {
+      method: method.toUpperCase(),
+      headers: this.headers
+    };
+
+    if (body) options.body = JSON.stringify(body);
+    if (authRequired) options.headers = this.authHeaders;
+
+    return options;
+  }
+}
+
+export default API;
