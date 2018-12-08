5757e15aa821963a144f36c25923a4a9191a20b6
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-08 17:55:11 +0100

    Make DraggableModules a stateless functional component

--- client/src/components/Modules/DraggableModules.jsx
+++ client/src/components/Modules/DraggableModules.jsx
@@ -1,62 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-class DraggableModules extends Component {
-  reorder = (list, startIndex, endIndex) => {
+const DraggableModules = ({ modules, finishReorder, renderModule }) => {
+  const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
     return result;
   };
 
-  onDragEnd = (result) => {
+  const onDragEnd = (result) => {
     if (!result.destination) {
       return;
     }
 
-    const { modules, finishReorder } = this.props;
-    const reordered = this.reorder(modules, result.source.index, result.destination.index);
+    const reordered = reorder(modules, result.source.index, result.destination.index);
     finishReorder(reordered);
-  }
-
-  renderModule = (provided, module, index) => {
-    const { renderModule } = this.props;
-    return (
-      <Draggable key={module._id} draggableId={module._id} index={index}>
-        {innerProvided => (
-          <div
-            ref={innerProvided.innerRef}
-            {...innerProvided.draggableProps}
-          >
-            {renderModule(module, innerProvided.dragHandleProps)}
-          </div>
-        )}
-      </Draggable>
-    );
   };
 
-  render() {
-    const { modules } = this.props;
+  const renderDraggable = (provided, module, index) => (
+    <Draggable key={module._id} draggableId={module._id} index={index}>
+      {innerProvided => (
+        <div
+          ref={innerProvided.innerRef}
+          {...innerProvided.draggableProps}
+        >
+          {renderModule(module, innerProvided.dragHandleProps)}
+        </div>
+      )}
+    </Draggable>
+  );
 
-    return (
-      <div className="modules">
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <Droppable droppableId="droppable">
-            {provided => (
-              <div ref={provided.innerRef}>
-                {modules.map((module, index) => (
-                  this.renderModule(provided, module, index)
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="modules">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {provided => (
+            <div ref={provided.innerRef}>
+              {modules.map((module, index) => (
+                renderDraggable(provided, module, index)
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
 
 DraggableModules.propTypes = {
   modules: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
