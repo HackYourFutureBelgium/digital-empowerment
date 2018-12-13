34c543f61b173bae31612cf8d7b6442788c4c310
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-13 10:52:51 +0100

    Add download link to DBSF report

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from '@blueprintjs/core';
 import Header from '../Header';
 import User from '../../models/User';
+
+import dbsfReport from '../../assets/dbsf-report.pdf';
 
 const Home = ({ user }) => (
   <div>
     <div className="container user-container">
       <Header user={user} />
+      <a href={dbsfReport} className="link--seamless" download><Button>download the DBSF report</Button></a>
     </div>
   </div>
 );
